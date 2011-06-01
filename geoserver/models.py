29b2abb9ddac4e4cb113d23ca9d8075b94f14623
Marco Montanari <sirmmo@gmail.com>
2011-06-01 16:57:57 +0000

    model

--- geoserver/models.py
+++ geoserver/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from uuid import uuid4
+from django.contrib.gis.gdal import *
+from django.contrib.gis.db import models as geomodels
 
 # Create your models here.
+
+class ShapeFile(models.Model):
+	name = models.TextField()
+	srs = models.TextField()
+
+class Instance(models.Model):
+	name = models.CharField(primary_key=True, max_length=64, editable=False, blank=True, default=uuid4)
+	fid = models.IntegerField()
+	shapefile = models.ForeignKey(ShapeFile)
+	position = geomodels.GeometryField()
+	objects = models.GeoManager()
+	
+	def save(self, *args, **kwargs):
+		if SpatialReference(self.shapefile.srs) != SpatialReference(4326):
+			ct = CoordTransform(SpatialReference(self.shapefile.srs), SpatialReference('WGS84'))
+			self.position.transform(ct)
+
+class Column(models.Model):
+	name = models.CharField(max_length=256)
+	value = models.TextField()
+	type = models.TextField()
